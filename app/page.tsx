cc4877377f7695575a880f8976f3a9af536e09f9
gokulnathpeace@gmail.com <gokulnathpeace@gmail.com>
2024-12-16 10:38:54 +0530

    Landing page , handled authorization

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,25 @@
 import { Button } from '@/components/ui/button'
+import { FlipWords } from '@/components/ui/flip-words'
 import Link from 'next/link'
 
 export default function Home() {
+  const words = ['Memories', 'Goals', 'Accomplisments', 'Reminder']
   return (
-    <div>
-      <Link href='/create-mail'>
-        <Button className='font-bricolage'>Ping your future self</Button>
-      </Link>
-      <Link href='/dashboard'>
-        <Button className='font-bricolage'>Dashboard</Button>
-      </Link>
+    <div className='min-h-[20rem] flex flex-col justify-center '>
+      <div className='h-max m-4 px-4'>
+        <div className='text-7xl font-normal text-text'>
+          Send
+          <FlipWords words={words} /> <br />
+          to your future self
+        </div>
+      </div>
+      <div className='m-4 px-4 flex gap-1'>
+        <Link href='/create-mail'>
+          <Button className='font-bricolage bg-yellow text-black text-lg p-4 font-normal hover:bg-[#ffc32b] '>
+            Ping your future self
+          </Button>
+        </Link>
+      </div>
     </div>
   )
 }
